7a1fc6756ac4cd9af0fdd554ad936bdd73abd3cd
Rain Hu <intervalrain@gmail.com>
2021-12-23 01:56:51 +0800

    update leetcode no.143

---
+++ src/main/java/com/rainhu/n143_ReorderList.java
@@ -0,0 +1,58 @@
+package com.rainhu;
+
+import com.rainhu.DioUtility.ListNode;
+
+/**
+ * 143. Reorder List (Medium)
+ * 
+ * You are given the head of a singly linked-list.
+ * The list can be represented as:
+ *     L0 -> L1 -> ... -> Ln-1 -> Ln
+ * Reorder the list to be on the following form:
+ *     L0 -> Ln -> L1 -> Ln-1 -> L2 -> Ln-2 -> ...
+ * You may not modify the values in the list's nodes.
+ * Only nodes themselves may be changed.
+ * 
+ * @author: Rain Hu
+ * @version: 1
+ * @since: 2021/12/23
+ * @apiNote: 1. Find the middle of the List
+ *           2. Reverse the half after middle
+ *           3. Start reorder one by one
+ */
+public class n143_ReorderList{
+    public void reorderList(ListNode head){
+        if (head == null || head.next == null) return;
+
+        // Find the middle of the List
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while (p2.next != null && p2.next.next != null){
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+
+        // Reverse the half after middle
+        ListNode mid = p1;
+        ListNode curr = p1.next;
+        while (curr.next != null){
+            ListNode tmp = curr.next;
+            curr.next = tmp.next;
+            tmp.next = mid.next;
+            mid.next = tmp;
+        }
+
+        // Start reorder one by one
+        p1 = head;
+        p2 = mid.next;
+        while (p1 != mid){
+            mid.next = p2.next;
+            p2.next = p1.next;
+            p1.next = p2;
+            p1 = p2.next;
+            p2 = mid.next;
+        }
+
+    }
+
+}
---
+++ src/test/java/com/rainhu/n143_ReorderListTest.java
@@ -0,0 +1,29 @@
+package com.rainhu;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.rainhu.DioUtility.ListNode;
+
+import org.junit.jupiter.api.Test;
+
+public class n143_ReorderListTest{
+
+    n143_ReorderList clz = new n143_ReorderList();
+    ListNode head;
+    ListNode expected;
+
+    @Test
+    public void test1(){
+        ListNode head = new ListNode(1,2,3,4);
+        expected = new ListNode(1,4,2,3);
+        clz.reorderList(head);
+        assertTrue(head.toString().equals(expected.toString()));
+    }
+
+    @Test void test2(){
+        ListNode head = new ListNode(1,2,3,4,5);
+        expected = new ListNode(1,5,2,4,3);
+        clz.reorderList(head);
+        assertTrue(head.toString().equals(expected.toString()));
+    }
+}
