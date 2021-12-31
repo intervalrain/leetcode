74fc5cff8da16b38006d0abd2d24d1749de4acee
Rain Hu <intervalrain@gmail.com>
2021-12-31 16:47:52 +0800

    update leetcode no.1026

---
+++ src/main/java/com/rainhu/n1026_MaximumDifferenceBetweenNodeAndAncestor.java
@@ -0,0 +1,39 @@
+package com.rainhu;
+
+import com.rainhu.DioUtility.TreeNode;
+
+/**
+ * 1026. Maximum Difference Between Node and Ancestor (Medium)
+ * 
+ * Given the root of a binary tree, 
+ * find the maximum value v for which there exist different nodes a and b where v = |a.val - b.val| and a is an ancestor of b.
+ * 
+ * A node a is an ancestor of b if either: any child of a is equal to b or any child of a is an ancestor of b.
+ */
+
+public class n1026_MaximumDifferenceBetweenNodeAndAncestor{
+    int maxDiff;
+    public int maxAncestorDiff(TreeNode root) {
+        if (root == null) return 0;
+        maxDiff = 0;
+        if (root.left != null) helper(root, root.left, 0, 0);
+        if (root.right != null) helper(root, root.right, 0, 0);
+        return maxDiff;
+    }
+
+    public void helper(TreeNode root, TreeNode child, int pos, int neg){
+        if (root.val - child.val + pos > 0)
+            pos += root.val - child.val;
+        else
+            pos = 0;
+        
+        if (root.val - child.val + neg < 0)
+            neg += root.val - child.val;
+        else
+            neg = 0;
+        maxDiff = Math.max(Math.max(pos, maxDiff), Math.max(-neg, maxDiff));
+
+        if (child.left != null) helper(child, child.left, pos, neg);
+        if (child.right != null) helper(child, child.right, pos, neg);
+    }
+}
---
+++ src/test/java/com/rainhu/n1026_MaximumDifferenceBetweenNodeAndAncestorTest.java
@@ -0,0 +1,31 @@
+package com.rainhu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.rainhu.DioUtility.TreeNode;
+
+import org.junit.jupiter.api.Test;
+
+public class n1026_MaximumDifferenceBetweenNodeAndAncestorTest{
+    
+    n1026_MaximumDifferenceBetweenNodeAndAncestor clz = new n1026_MaximumDifferenceBetweenNodeAndAncestor();
+    TreeNode root;
+    int expected;
+    int actual;
+
+    @Test
+    public void test1(){
+        root = TreeNode.arrayToTree(new Integer[]{8,3,10,1,6,null,14,null,null,4,7,13});
+        expected = 7;
+        actual = clz.maxAncestorDiff(root);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2(){
+        root = TreeNode.arrayToTree(new Integer[]{1,2,null,0,null,null,null,3});
+        expected = 3;
+        actual = clz.maxAncestorDiff(root);
+        assertEquals(expected, actual);
+    }
+}
