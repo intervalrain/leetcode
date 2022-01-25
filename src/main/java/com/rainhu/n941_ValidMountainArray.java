ec0991a75cdeaed89ad2df0f5d760d0868f220e1
Rain Hu <intervalrain@gmail.com>
2022-01-25 21:38:30 +0800

    fix leetcode no.941

--- src/main/java/com/rainhu/n941_ValidMountainArray.java
+++ src/main/java/com/rainhu/n941_ValidMountainArray.java
@@ -18,9 +18,9 @@ package com.rainhu;
  */
 
 public class n941_ValidMountainArray{
-    public boolean validMountainArray(int[] arr){
-        if (arr.length < 3)
-	    	return false;
+	public boolean validMountainArray(int[] arr){
+		if (arr.length < 3)
+			return false;
 		int i = 0;
 		while (i < arr.length - 1){
 			if (arr[i] > arr[i + 1])
@@ -37,5 +37,5 @@ public class n941_ValidMountainArray{
 			i++;
 		}
 		return true;
-    }
+	}
 }
