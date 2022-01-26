c3f00ab5388d5f8d1f8860adbd74f8af8704495e
Rain Hu <intervalrain@gmail.com>
2022-01-27 00:07:20 +0800

    update leetcode no.1305

--- README.md
+++ README.md
@@ -26,6 +26,10 @@
 - BFS
     - [1291. Sequential Digits](https://github.com/intervalrain/leetcode/blob/master/src/main/java/com/rainhu/n1291_SequentialDigits.java)
 
+- DFS
+    - [1305. All Elements in Two Binary Search Trees](https://github.com/intervalrain/leetcode/blob/master/src/main/java/com/rainhu/n1305_AllElementsInTwoBinaryTree.java)
+
+
 - Unsorted
     
     - [5. Longest Palindromic Substring](https://github.com/intervalrain/leetcode/blob/master/src/main/java/com/rainhu/n5_LongestPalindromicSubstring.java)
---
+++ src/test/java/com/rainhu/n1305_AllElementsInTwoBinarySearchTreesTest.java
@@ -0,0 +1,37 @@
+package com.rainhu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import com.rainhu.DioUtility.TreeNode;
+
+import org.junit.jupiter.api.Test;
+
+public class n1305_AllElementsInTwoBinarySearchTreesTest{
+
+    n1305_AllElementsInTwoBinarySearchTrees clz = new n1305_AllElementsInTwoBinarySearchTrees();
+    TreeNode root1;
+    TreeNode root2;
+    List<Integer> expected;
+    List<Integer> actual;
+
+    @Test
+    public void test1(){
+        root1 = TreeNode.arrayToTree(new Integer[]{2,1,4});
+        root2 = TreeNode.arrayToTree(new Integer[]{1,0,3});
+        expected = List.of(0,1,1,2,3,4);
+        actual = clz.getAllElements(root1, root2);
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    public void test2(){
+        root1 = TreeNode.arrayToTree(new Integer[]{1,null,8});
+        root2 = TreeNode.arrayToTree(new Integer[]{8,1});
+        expected = List.of(1,1,8,8);
+        actual = clz.getAllElements(root1, root2);
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+}
