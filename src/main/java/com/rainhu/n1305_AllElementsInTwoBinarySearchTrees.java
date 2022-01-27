a28c78ea841b78af7fc3ce461008e3b5c2aeedfe
Rain Hu <intervalrain@gmail.com>
2022-01-28 03:49:55 +0800

    update leetcode no.1305

--- src/main/java/com/rainhu/n1305_AllElementsInTwoBinarySearchTrees.java
+++ src/main/java/com/rainhu/n1305_AllElementsInTwoBinarySearchTrees.java
@@ -3,6 +3,7 @@ package com.rainhu;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import com.rainhu.DioUtility.TreeNode;
 
@@ -13,43 +14,44 @@ import com.rainhu.DioUtility.TreeNode;
  * return a list containing all the integers from both trees sorted in ascending order.
  * 
  * @author: Rain Hu
- * @version: 1
+ * @version: 2
  * @since: 2022/1/26
- * @apiNote: Tree
+ * @apiNote: dfs
  */
 
 public class n1305_AllElementsInTwoBinarySearchTrees{
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        LinkedList<Integer> ret = new LinkedList<>();
-        List<Integer> ret2 = new ArrayList<>();
-        
-        dfs(root1, ret);
-        dfs2(root2, ret2, ret);
-        
-        if (ret.size() > 0)
-            ret2.addAll(ret);
-        return ret2;
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> res = new ArrayList<>();
+
+        dfs(root1, q);
+        dfs2(root2, res, q);
+
+        if (!q.isEmpty())
+            res.addAll(q);
+
+        return res;
     }
-    
-    private void dfs(TreeNode node, LinkedList<Integer> ret) {
-        if (node == null)
+
+    private void dfs(TreeNode root, Queue<Integer> q){
+        if (root == null)
             return;
-        if (node.left != null)
-            dfs(node.left, ret);
-        ret.add(node.val);
-        if (node.right != null)
-            dfs(node.right, ret);
+        if (root.left != null)
+            dfs(root.left, q);
+        q.add(root.val);
+        if (root.right != null)
+            dfs(root.right, q);
     }
-    
-    private void dfs2(TreeNode node, List<Integer> ret, LinkedList<Integer> ex) {
-        if (node == null)
+
+    private void dfs2(TreeNode root, List<Integer> list, Queue<Integer> q){
+        if (root == null)
             return;
-        if (node.left != null)
-            dfs2(node.left, ret, ex);
-        while (!ex.isEmpty() && ex.peekFirst() < node.val)
-            ret.add(ex.pollFirst());
-        ret.add(node.val);
-        if (node.right != null)
-            dfs2(node.right, ret, ex);
+        if (root.left != null)
+            dfs2(root.left, list, q);
+        while (!q.isEmpty() && q.peek() < root.val)
+            list.add(q.poll());
+        list.add(root.val);
+        if (root.right != null)
+            dfs2(root.right, list, q);
     }
 }
