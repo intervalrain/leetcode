f677f48edd78892a6db67cafd0bc096954088654
Rain Hu <intervalrain@gmail.com>
2022-03-05 00:04:36 +0800

    update leetcode no. 799

---
+++ src/main/java/com/rainhu/n799_ChampagneTower.java
@@ -0,0 +1,46 @@
+package com.rainhu;
+
+/**
+ * 799. Champagne Tower (Medium)
+ * 
+ * We stack glasses in a pyramid, 
+ * where the first row has 1 glass, 
+ * the second row has 2 glasses, 
+ * and so on until the 100th row.
+ * Each glass holds one cup of champagne.
+ * Then, some champagne is poured into the first glass at the top.
+ * When the topmost glass is full,
+ * any excess liquid poured will fall equally to the glass immediately to the left and right of it.
+ * When those glasses become full, 
+ * any excess champagne will fall equally to the left and right of those glasses, and so on.  
+ * (A glass at the bottom row has its excess champagne fall on the floor.)
+ * 
+ * For example, after one cup of champagne is poured,
+ * the top most glass is full.
+ * After two cups of champagne are poured,
+ * the two glasses on the second row are half full. 
+ * After three cups of champagne are poured,
+ * those two cups become full - there are 3 full glasses total now. 
+ * After four cups of champagne are poured,
+ * the third row has the middle glass half full
+ * and the two outside glasses are a quarter full.
+ * 
+ * Now after pouring some non-negative integer cups of champagne,
+ * return how full the jth glass in the ith row is (both i and j are 0-indexed.)
+ * 
+ * @author: Rain Hu
+ * @version: 1
+ * @since: 2022/3/4
+ * @apiNote:
+ */
+
+public class n799_ChampagneTower {
+    public double champagneTower(int poured, int query_row, int query_glass){
+        double[] res = new double[query_row + 2];
+        res[0] = poured;
+        for (int row = 1; row <= query_row; row++)
+            for (int i = row; i >= 0; i--)
+                res[i + 1] += res[i] = Math.max(0.0, (res[i] - 1) / 2);
+        return Math.min(res[query_glass], 1.0);
+    }
+}
---
+++ src/test/java/com/rainhu/n799_ChampagneTowerTest.java
@@ -0,0 +1,42 @@
+package com.rainhu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class n799_ChampagneTowerTest {
+    
+    n799_ChampagneTower clz = new n799_ChampagneTower();
+    int poured, query_row, query_glass;
+    double expected, actual;
+
+    @Test
+    public void test1(){
+        poured = 1;
+        query_row = 1;
+        query_glass = 1;
+        expected = 0.00000;
+        actual = clz.champagneTower(poured, query_row, query_glass);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2(){
+        poured = 2;
+        query_row = 1;
+        query_glass = 1;
+        expected = 0.50000;
+        actual = clz.champagneTower(poured, query_row, query_glass);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test3(){
+        poured = 100000009;
+        query_row = 33;
+        query_glass = 17;
+        expected = 1;
+        actual = clz.champagneTower(poured, query_row, query_glass);
+        assertEquals(expected, actual);
+    }
+}
