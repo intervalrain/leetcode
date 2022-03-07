ecfa03f2e1494e2650aebf6710ec5291cc49db1b
Rain Hu <intervalrain@gmail.com>
2022-03-07 15:15:46 +0800

    update test for leetcode no. 21

--- src/main/java/com/rainhu/n21_MergeTwoSortedLists.java
+++ src/main/java/com/rainhu/n21_MergeTwoSortedLists.java
@@ -7,53 +7,33 @@ import com.rainhu.DioUtility.ListNode;
  * 
  * Merge two sorted linked lists and return it as a sorted lists.
  * The list should be made by splicing together the noeds of the first two lists.
+ * 
+ * @author: Rain Hu
+ * @version: 2
+ * @since: 2022/3/7
+ * @apiNote: LinkedList
  */
 
 public class n21_MergeTwoSortedLists{
 
-    public static ListNode mergeTwoLists(ListNode l1, ListNode l2){
-        if(l1 == null)
-            return l2;
-        else if(l2 == null)
-            return l1;
-
-        ListNode p = l1;
-        ListNode q = l2;
-        ListNode head = new ListNode(0);
-        ListNode run = head;
-        
-        while(p !=null && q != null){
-            if (p.val < q.val){
-                run.next = p;
-                run = run.next;
-                if(p.next == null){
-                    p.next = q;
-                    break;
-                }
-                p = p.next;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val){
+                curr.next = l1;
+                l1 = l1.next;
             } else {
-                run.next = q;
-                run = run.next;
-                if(q.next == null){
-                    q.next = p;
-                    break;
-                }
-                q = q.next;
+                curr.next = l2;
+                l2 = l2.next;
             }
+            curr = curr.next;
         }
-        
-        return head.next;
-    }
-    public static void main(String[] args){
-        ListNode l1 = new ListNode(1, 2, 4);
-        ListNode l2 = new ListNode(1, 3, 4);
-
-        ListNode l = mergeTwoLists(l1, l2);
-        System.out.print(l.val);
-        while(l.next != null){
-            l = l.next;
-            System.out.print(l.val);
+        if (l1 == null){
+            curr.next = l2;
+        } else {
+            curr.next = l1;
         }
-        System.out.println();
+        return dummy.next;
     }
 }
---
+++ src/test/java/com/rainhu/n21_mergeTwoListsTest.java
@@ -0,0 +1,43 @@
+package com.rainhu;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.rainhu.DioUtility.ListNode;
+
+import org.junit.jupiter.api.Test;
+
+public class n21_mergeTwoListsTest {
+    
+    n21_MergeTwoSortedLists clz = new n21_MergeTwoSortedLists();
+    ListNode l1, l2;
+    ListNode expected, actual;
+
+    @Test
+    public void test1(){
+        l1 = new ListNode(1,2,4);
+        l2 = new ListNode(1,3,4);
+        expected = new ListNode(1,1,2,3,4,4);
+        actual = clz.mergeTwoLists(l1, l2);
+        assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    public void test2(){
+        l1 = null;
+        l2 = null;
+        expected = null;
+        actual = clz.mergeTwoLists(l1, l2);
+        assertNull(actual);
+    }
+
+    @Test
+    public void test3(){
+        l1 = null;
+        l2 = new ListNode(0);
+        expected = l2;
+        actual = clz.mergeTwoLists(l1, l2);
+        assertTrue(actual.equals(expected));
+        
+    }
+}
