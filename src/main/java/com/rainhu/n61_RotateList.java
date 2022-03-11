cf55ba664497f438ca27654164ac5e7c6acebf12
Rain Hu <intervalrain@gmail.com>
2022-03-12 00:17:21 +0800

    update leetcode no.61

---
+++ src/main/java/com/rainhu/n61_RotateList.java
@@ -0,0 +1,39 @@
+package com.rainhu;
+
+import com.rainhu.DioUtility.ListNode;
+
+/**
+ * 61. Rotate List (Medium)
+ * 
+ * Given the head of a linked list, rotate the list to the right by k places
+ */
+
+public class n61_RotateList {
+    ListNode rotateRight(ListNode head, int k){
+        if (head == null || k == 0) return head;
+        ListNode curr = head;
+        // int n = head.size();
+        int n = 0;
+        while (curr != null){
+            curr = curr.next;
+            n++;
+        }
+        curr = head;
+        k = k > n ? (n - (k % n)) : n - k;
+        if (k == 0) return head;
+        k--;
+        while (k > 0){
+            curr = curr.next;
+            k--;
+        }
+        ListNode newHead = curr.next;
+        curr.next = null;
+        curr = newHead;
+        if (curr == null) return head;
+        while (curr.next != null){
+            curr = curr.next;
+        }
+        curr.next = head;
+        return newHead;
+    }   
+}
---
+++ src/test/java/com/rainhu/n61_RotateListTest.java
@@ -0,0 +1,61 @@
+package com.rainhu;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.rainhu.DioUtility.ListNode;
+
+import org.junit.jupiter.api.Test;
+
+public class n61_RotateListTest {
+
+    n61_RotateList clz = new n61_RotateList();
+    ListNode head;
+    int k;
+    ListNode expected, actual;
+
+    @Test
+    public void test1(){
+        head = new ListNode(1,2,3,4,5);
+        k = 2;
+        expected = new ListNode(4,5,1,2,3);
+        actual = clz.rotateRight(head, k);
+        assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    public void test2(){
+        head = new ListNode(0,1,2);
+        k = 4;
+        expected = new ListNode(2,0,1);
+        actual = clz.rotateRight(head, k);
+        assertTrue(expected.equals(actual));
+    }
+
+    @Test 
+    public void test3(){
+        head = null;
+        k = 1;
+        expected = null;
+        actual = clz.rotateRight(head, k);
+        assertNull(actual);
+    }
+
+    @Test
+    public void test4(){
+        head = new ListNode(1);
+        k = 0;
+        expected = head;
+        actual = clz.rotateRight(head, k);
+        assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    public void test5(){
+        head = new ListNode(1);
+        k = 99;
+        expected = head;
+        actual = clz.rotateRight(head, k);
+        assertTrue(expected.equals(actual));
+    }
+}
